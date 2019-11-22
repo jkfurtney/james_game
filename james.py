94d80383738336777c260c4aad1743665623b23c
Jason Furtney <jkfurtney@gmail.com>
2019-11-22 14:59:51 -0600

    basic mouse following

--- james.py
+++ james.py
@@ -1,8 +1,12 @@
-import sys, pygame
+import sys
+import pygame
+from math import sqrt
+
 pygame.init()
 clock = pygame.time.Clock()
 size = width, height = 640, 480
-speed = [2, 2]
+speed_magnitude = 20
+speed = [speed_magnitude, speed_magnitude]
 black = 0, 0, 0
 
 screen = pygame.display.set_mode(size)
@@ -13,7 +17,15 @@ ballrect = ball.get_rect()
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
-    print( pygame.mouse.get_pos())
+    mx, my = pygame.mouse.get_pos()
+    if pygame.mouse.get_focused():
+        bx, by = ballrect.center
+        sx = mx - bx
+        sy = my - by
+        magnitude = sqrt(sx**2 + sy**2)
+        sx = speed_magnitude * sx / magnitude
+        sy = speed_magnitude * sy / magnitude
+        speed = [sx, sy]
     ballrect = ballrect.move(speed)
     if ballrect.left < 0 or ballrect.right > width:
         speed[0] = -speed[0]
